cdfab1a6455fd42d538e5a47ae4ad6d83063d1ac
Tommaso Peritore <tommasoperitoree@gmail.com>
2024-06-13 01:00:10 +0200

    update lesson 9

--- LSN_09/9.1/SOURCE/city.cpp
+++ LSN_09/9.1/SOURCE/city.cpp
@@ -3,11 +3,27 @@
 
 #include "city.h"
 
+
+/**
+ * @brief Default constructor for the City class.
+ * 
+ * This function initializes the location vector of the city.
+ * 
+ */
 City :: City (){
    _location.resize(_dimension);
    return ;
 }
 
+/**
+ * @brief Sets the location of the city.
+ * 
+ * This function sets the location of the city using the provided vector and dimension.
+ * sIf the dimension is not compatible with the city's dimension, an error message is printed.
+ * 
+ * @param location The vector representing the location of the city.
+ * @param dimension The dimension of the location vector.
+ */
 void City :: set_location (vec location, int dimension) {
 	if (dimension == _dimension)
 		_location = location ;
@@ -15,6 +31,13 @@ void City :: set_location (vec location, int dimension) {
 	return ;
 }
 
+/**
+ * @brief Returns the location of the city.
+ * 
+ * This function returns the location of the city as a vector.
+ * 
+ * @return The location of the city as a vector.
+ */
 vec City :: return_location () {
 	return _location ;
 }
--- LSN_09/9.1/SOURCE/city.h
+++ LSN_09/9.1/SOURCE/city.h
@@ -5,18 +5,38 @@
 
 #include "random.h"
 
-
 using namespace std;
 using namespace arma;
 
+/**
+ * @brief Represents a city in a two-dimensional system.
+ * 
+ * The City class stores the location of a city in a two-dimensional system.
+ * It provides methods to set and retrieve the location of the city.
+ */
 class City {
-
 	private:
-		const int _dimension = 2;															// Dimensionality of the system
-		vec _location;
+		const int _dimension = 2;	// Dimensionality of the system
+		vec _location;				// Location of the city
 
 	public:
-		City () ;
-		void set_location (vec location, int dimension) ;
-		vec return_location ();
+		/**
+		 * @brief Default constructor for the City class.
+		 */
+		City();
+
+		/**
+		 * @brief Sets the location of the city.
+		 * 
+		 * @param location The location of the city as a vector.
+		 * @param dimension The dimensionality of the system.
+		 */
+		void set_location(vec location, int dimension);
+
+		/**
+		 * @brief Returns the location of the city.
+		 * 
+		 * @return The location of the city as a vector.
+		 */
+		vec return_location();
 };
